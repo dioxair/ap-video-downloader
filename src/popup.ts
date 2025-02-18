d4d54ec86d82ba42558cb00e7dd9a6f6cede8ad4
Samuel Olagunju <me@samola.net>
2025-02-17 20:58:46 -0500

    Added a button for downloading video subtitles

--- src/popup.ts
+++ src/popup.ts
@@ -1,6 +1,9 @@
 const downloadButton = document.getElementById(
   "downloadVideoButton",
 ) as HTMLButtonElement;
+const downloadSubtitlesButton = document.getElementById(
+  "downloadSubsButton",
+) as HTMLButtonElement;
 const openTabButton = document.getElementById(
   "openVideoInTabButton",
 ) as HTMLButtonElement;
@@ -20,21 +23,66 @@ function getMonitoringState(callback: (url: string | null) => void) {
   });
 }
 
-function downloadVideo(url: string, filename: string) {
-  const a = document.createElement("a");
-  a.href = url;
-  a.download = filename;
-  a.click();
+async function getCurrentUrl(): Promise<string | null> {
+  const [tab] = await chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true,
+  });
+  if (tab.url === undefined) return null;
+  return tab.url;
+}
+
+function getVideoID(url: string): string | null {
+  return new URLSearchParams(new URL(url).search).get("apd");
+}
+
+async function downloadFile(url: string, filename: string) {
+  const response = await fetch(url);
+  const blob = await response.blob();
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(blob);
+  link.download = filename;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  URL.revokeObjectURL(link.href);
+}
+
+function getSubtitlesUrl(url: string): string | null {
+  const videoID = getVideoID(url);
+
+  if (videoID) {
+    return `https://fast.wistia.net/embed/captions/${videoID}.vtt?language=eng`;
+  } else {
+    return null;
+  }
 }
 
 downloadButton.addEventListener("click", () => {
-  getVideoUrl((videoUrl) => {
+  getVideoUrl(async (videoUrl) => {
     if (videoUrl) {
-      downloadVideo(videoUrl, "video.mp4");
+      await downloadFile(videoUrl, "video.mp4");
     } else {
       alert("No video URL detected yet.");
     }
   });
+});
+
+downloadSubtitlesButton.addEventListener("click", async () => {
+  const currentUrl = await getCurrentUrl();
+  if (currentUrl === null) {
+    console.log("Could not find current tab URL");
+    return;
+  }
+
+  if (!getVideoID(currentUrl)) {
+    alert("Please navigate to an AP Classroom video.");
+  }
+
+  const subtitlesUrl = getSubtitlesUrl(currentUrl);
+  if (subtitlesUrl !== null) {
+    await downloadFile(subtitlesUrl, "subtitles.vtt");
+  }
 });
 
 openTabButton.addEventListener("click", () => {
