aaca8a954d1f774d9c02ffffc085d69221ee5c6b
Samuel Olagunju <me@samola.net>
2025-09-16 10:02:12 -0400

    Created DownloadService
    maybe I should chill on the services..

---
+++ src/classes/DownloadService.ts
@@ -0,0 +1,13 @@
+export class DownloadService {
+  async downloadFile(url: string, filename: string): Promise<void> {
+    const response = await fetch(url);
+    const blob = await response.blob();
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(link.href);
+  }
+}
